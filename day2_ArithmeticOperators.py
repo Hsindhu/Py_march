10933fc198bed2fbf8c0c51789765a59be844fea
SINDHU HOTHURU <hsindhu.111@gmail.com>
2022-03-29 17:47:58 -0500

    Added 2 files and modified the demo.txt

---
+++ day2_ArithmeticOperators.py
@@ -0,0 +1,29 @@
+num_1 = 3
+num_2 = 2
+
+sum = num_1 + num_2
+print(f'The addition of {num_1} and {num_2} is : {sum}')
+
+
+substract = num_1 - num_2
+print(f'The substration of {num_1} and {num_2} is : {substract}')
+
+
+multiply = num_1 * num_2
+print(f'The multiplication of {num_1} and {num_2} is : {multiply}')
+
+
+divide = num_1 / num_2
+print(f'The division of {num_1} and {num_2} is : {divide}')
+
+
+floor_divide = num_1 // num_2
+print(f'The floor_division of {num_1} and {num_2} is : {floor_divide}')
+
+
+exponent = num_1 ** num_2
+print(f'The exponential of {num_1} and {num_2} is : {exponent}')
+
+
+modulus = num_1 % num_2
+print(f'The modulus of {num_1} and {num_2} is : {modulus}')
